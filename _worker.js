193bb4b2c8ae6de649944c18ae06bd710dd56da5
irxz051 <167430987+irxz051@users.noreply.github.com>
2024-04-30 12:29:18 +0330

    Update _worker.js
    test

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = ' 4498382f-3dbd-445d-a602-5f99338c0190 ';
+let userID = '4498382f-3dbd-445d-a602-5f99338c0190';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
